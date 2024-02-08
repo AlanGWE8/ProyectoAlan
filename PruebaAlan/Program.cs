858918023c9d5d56437598f47b4f8b20ad8c4970
edwin.gongora <edwin.gongora@MXCDP4E11>
2024-02-08 16:05:47 -0500

    Agregar archivos de proyecto.

---
+++ PruebaAlan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PruebaAlan
+{
+    /// <summary>
+    /// Programa para ejecutar proceso de comisiones.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Alan");
+            Console.ReadLine();
+        }
+    }
+}
