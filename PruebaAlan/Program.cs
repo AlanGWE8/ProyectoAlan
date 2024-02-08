6cc1fb5ec0023814b3e83383aea04eaed3626ec0
Alan Edwin Gongora Valez <edwin.gongora@cun.pth>
2024-02-08 17:32:29 -0500

    Prueba 1

--- PruebaAlan/Program.cs
+++ PruebaAlan/Program.cs
@@ -10,7 +10,14 @@ namespace PruebaAlan
         public static void Main(string[] args)
         {
             Console.WriteLine("Hola Alan");
+
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
             Console.ReadLine();
+            /// EJERCICIO 5///
         }
+
     }
 }
+
